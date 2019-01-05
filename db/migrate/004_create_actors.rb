e32a96fb1ae62b22c2c14d820e7e3523064f66f4
Priscilla H <priscillamhuynh@gmail.com>
2019-01-05 06:51:02 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -3,6 +3,7 @@ class CreateActors < ActiveRecord::Migration[4.2]
     create_table :actors do |a|
       a.string :first_name
       a.string :last_name
+      a.integer :character_id
     end
   end
 end
